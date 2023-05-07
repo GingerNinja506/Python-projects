bfeaaa138c61e2e06f389c33436d3d6e28ef9130
Mateusz Rudy <109037258+GingerNinja506@users.noreply.github.com>
2023-05-07 17:56:28 +0200

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,16 +1,40 @@
 import random
-def guess(x):
-    random_number = random.randint(1,x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f"Guess the number from 1 to {x}  "))
-        if guess < random_number:
-            print("Number is to low")
-        elif guess > random_number:
-            print("Sorry number is to high")
-        else:
-            print("Yay u made it")
-guess(10)
+def game():
+    game_on = True
+    
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    lives = 0
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if level == "easy":
+        lives = 10
+        print(f"You have {lives} attempts remaining to guess the number.")
+    elif level == "hard":
+        lives = 5
+        print(f"You have {lives} attempts remaining to guess the number.")
+    number = random.randint(0,100)
+    while game_on == True:
+        guess = int(input("Guess a number : "))
+    
+
+        if guess != number and guess > number:
+                lives -= 1
+                print("Too high")
+                print("Guess again.")
+                print(f"You have {lives} attempts remaining to guess the number.")
+        elif guess != number and guess < number:
+                lives -= 1
+                print("Too low")
+                print("Guess again.")
+                print(f"You have {lives} attempts remaining to guess the number.")
+        elif guess == number:
+                print(f"You got it! The answer was {number} ")
+                game_on = False
+
+    
+game()
+    
+    
     
     
         
